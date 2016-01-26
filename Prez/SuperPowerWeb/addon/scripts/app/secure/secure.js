d17d2a1757956ca2eda7d705e1cdebefb6e96746
jefBinomed <jean.francois.garreau@gmail.com>
2016-01-26 13:49:38 +0100

    sauvegarde du pwd pour eviter d'avoir a le resaissir tout le temps

--- Prez/SuperPowerWeb/addon/scripts/app/secure/secure.js
+++ Prez/SuperPowerWeb/addon/scripts/app/secure/secure.js
@@ -1,10 +1,10 @@
 'use strict'
 
 function calculateAddress(){
-	if (location.port && (location.port === "3010")){
-		return "http://localhost:9000"
-	}else if (location.port && location.port === "9000"){
-		return "http://jef.binomed.fr:9000";
+	if (location.port && (location.port === "3000")){
+		return "http://localhost:8000"
+	}else if (location.port && location.port === "8000"){
+		return "http://jef.binomed.fr:8000";
 	}else{
 		return null;	
 	} 
@@ -12,34 +12,41 @@ function calculateAddress(){
 
 var address = calculateAddress();
 
+function doRequest($mdDialog, context, pwd){
+	let myHeaders = new Headers();
+	let myInit = { method: 'GET',
+           headers: myHeaders,
+           mode: 'cors',
+           cache: 'default' };
+
+	let myRequest = new Request(`${address}/password/${pwd}`,myInit);
+	fetch(myRequest)
+	.then(function(response){
+		return response.json();
+	})
+	.then(function(json){
+		// On ne retraire pas une question déjà traitée
+		if (json.auth){
+			localStorage['pwd'] = pwd;
+			$mdDialog.hide();
+		}else{
+			context.notvalid = true;
+		}
+
+
+	});
+}
+
 function SecureCtrl($mdDialog){
 	
 	this.notvalid = false;
 
 	this.try = function(){
-		let myHeaders = new Headers();
-		let myInit = { method: 'GET',
-	           headers: myHeaders,
-	           mode: 'cors',
-	           cache: 'default' };
-	    let context = this;
+		doRequest($mdDialog, this, this.pwd);
+	}
 
-		let myRequest = new Request(`${address}/password/${this.pwd}`,myInit);
-		fetch(myRequest)
-		.then(function(response){
-			return response.json();
-		})
-		.then(function(json){
-			// On ne retraire pas une question déjà traitée
-			if (json.auth){
-				$mdDialog.hide();
-			}else{
-				context.notvalid = true;
-			}
-
-
-		});
-
+	if (localStorage['pwd']){
+		doRequest($mdDialog, this, localStorage['pwd']);
 	}
 }
 
