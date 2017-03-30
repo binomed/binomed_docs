f4ae91502a3c206878de83166743342e56287cd3
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2017-03-30 18:44:26 +0200

    ajout prezs

---
+++ Prez/RTTSiteswap/initLinks.bat
@@ -0,0 +1,18 @@
+
+cd ..\..
+set WORKING_DIRECTORY=%cd%
+echo "Set Link to Theme"
+MKLINK /D %WORKING_DIRECTORY%\Prez\RTTSiteswap\css\theme %WORKING_DIRECTORY%\Themes\css\theme
+echo "Set Link to Theme Scss"
+MKLINK /D %WORKING_DIRECTORY%\Prez\RTTSiteswap\scss\theme %WORKING_DIRECTORY%\Themes\scss\theme
+echo "Set Link to reveal"
+MKLINK /D %WORKING_DIRECTORY%\Prez\RTTSiteswap\reveal %WORKING_DIRECTORY%\Tools\reveal
+echo "Set Link to sockets-notes"
+MKLINK /D %WORKING_DIRECTORY%\Prez\RTTSiteswap\libs\sockets-notes %WORKING_DIRECTORY%\Tools\sockets-notes
+echo "Set Link to reveal theme bootstrap"
+MKLINK /D %WORKING_DIRECTORY%\Prez\RTTSiteswap\libs\reveal-md-style-bootstrap %WORKING_DIRECTORY%\Tools\reveal-md-style-bootstrap
+echo "Set Link to HighlightJS"
+MKLINK /D %WORKING_DIRECTORY%\Prez\RTTSiteswap\libs\highlight.js %WORKING_DIRECTORY%\Tools\highlight.js
+echo "Set Link to HighlightJS Fix for reveal"
+MKLINK /H %WORKING_DIRECTORY%\Prez\RTTSiteswap\libs\highlight_reveal_fix_v8.0.js %WORKING_DIRECTORY%\Tools\highlight_reveal_fix_v8.0.js
+cd Prez\RTTSiteswap
