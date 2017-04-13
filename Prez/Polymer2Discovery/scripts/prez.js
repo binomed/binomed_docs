f940c9eb32b87b087f7913a9e9e15634cdea979c
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2017-04-13 11:03:49 +0200

    ajout slides

--- Prez/Polymer2Discovery/scripts/prez.js
+++ Prez/Polymer2Discovery/scripts/prez.js
@@ -9,8 +9,24 @@ import {
 
     function pageLoad() {
         new RevealEngineEvents();
+
+        _manageTimer();
+
     }
 
+    function _manageTimer() {
+
+        let startTimer = false;
+        Reveal.addEventListener('slidechanged', (event) => {
+            console.log(event);
+            if(event.indexh > 0 && !startTimer){
+                startTimer = true;
+                document.querySelector('gdg-timer').toggle = true;
+            }else if(event.indexh === 0 && startTimer){
+                startTimer = false;
+            }
+        });
+    }
 
     window.addEventListener('load', pageLoad);
 })();
