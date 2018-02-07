f455384bf2d1b14db1256fe5d4f9fadd31d8792f
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2018-02-07 14:30:31 +0100

    add fonts and start to play with build

---
+++ Prez/BleedingWeb/scripts/prez.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// import { MaskHighlighter } from '../node_modules/mask-highlighter/mask-highlighter.js';
+
+
+(async function () {
+
+
+    async function pageLoad() {
+
+    }
+
+
+    window.addEventListener('load', pageLoad);
+})();
