a83e82c4f11f5b7a3399f5b0d7591ef428eb1358
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2018-09-19 23:56:54 +0200

    refactor code to match last version of samples (but crash chrome)

--- Prez/HoudiniCss/scripts/houdini/animation-header.js
+++ Prez/HoudiniCss/scripts/houdini/animation-header.js
@@ -13,90 +13,34 @@ export class AnimationHeader{
 			  initialValue: '0px',
 			  inherits: true
 			});
-		  });
+			});
+		//let animationWorklet = window.animationWorklet;
+		//if (CSS)
+			//animationWorklet = CSS.animationWorklet;
 
-		await animationWorklet.addModule('./scripts/houdini/animator-header.js');
-
-		const sizes = document.querySelector('#demoAnimationWorklet').computedStyleMap();
-		const scrollSource = document.querySelector('#demoAnimationWorklet');
-		const avatar = document.querySelector('#demoAnimationWorklet .avatar');
-		const bar = document.querySelector('#demoAnimationWorklet .bar');
-		const maxTime = 1000;
-		const epsilon = 1e-2;
-		const scrollTimeline = new ScrollTimeline({
-		  scrollSource,
-		  orientation: 'block',
-		  timeRange: maxTime,
+		this.sizes = document.querySelector('#demoAnimationWorklet').computedStyleMap();
+		this.scrollSource = document.querySelector('#demoAnimationWorklet');
+		this.avatar = document.querySelector('#demoAnimationWorklet .avatar');
+		this.bar = document.querySelector('#demoAnimationWorklet .bar');
+		this.maxTime = 1000;
+		this.epsilon = 1e-2;
+		this.scrollTimeline = new ScrollTimeline({
+			scrollSource: this.scrollSource,
+			orientation: 'block',
+			timeRange: this.maxTime,
 		});
-		// console.log(avatarScrollEndPos, avatarScrollEndOffset);
-		// console.log(scrollSize);
-		const avatarEffect = new KeyframeEffect(avatar, [
-		  {transform: `translateY(0px) scale(1)`, easing: 'ease-in-out', offset: 0},
-		  {transform: `translateY(0px) scale(${0/*avatarTargetScale*/})`, easing: 'linear', offset: 0 /*avatarScrollEndOffset*/},
-		  {transform: `translateY(${0/*avatarTargetTranslate*/}px) scale(${0/*avatarTargetScale*/})`, offset: 1},
-		], {
-		  duration: maxTime + epsilon,
-		  fill: 'both',
-		  iterations: Infinity,
-		});
+		try{
 
-		new WorkletAnimation('animator-header',
-		  [avatarEffect],
-		  scrollTimeline,
-		  {}
-		).play();
 
-		const barEffect = new KeyframeEffect(
-		  bar,
-		  [
-			{opacity: 0, offset: 0},
-			{opacity: 1, offset: 0 /*avatarScrollEndOffset*/},
-			{opacity: 1, offset: 1}
-		  ],
-		  {
-			duration: maxTime + epsilon,
-			fill: 'both',
-			iterations: Infinity,
-			/* crbug(779189): Use infinity iteration and maxDuration to avoid effect
-				prematurely finishing.
+			await animationWorklet.addModule('./scripts/houdini/animator-header.js');
 
-				BTW, Web Animations uses an endpoint-exclusive timing model, which mean
-				when timeline is at "duration" time, it is considered to be at 0 time of the
-				second iteration. To avoid this, we ensure our max time (max scroll offset) never
-				reaches duration by having duration an epsilon larger.  This hack is not
-				needed once we fix the original bug above.
-			  */
-		  }
-		);
-		new WorkletAnimation('animator-header',
-		  [barEffect],
-		  scrollTimeline,
-		  {}
-		).play();
-
-		function update() {
-		  const clientHeight = scrollSource.clientHeight;
-		  const scrollHeight = scrollSource.scrollHeight;
-		  const maxScroll = scrollHeight - clientHeight;
-		  console.log(clientHeight, scrollHeight, maxScroll);
-		  const avatarTargetScale = sizes.get('--bar-height').value / (sizes.get('--avatar-size').value + 2 * sizes.get('--avatar-border').value);
-		  const avatarScrollEndPos = (sizes.get('--header-height').value/2 - sizes.get('--avatar-size').value/2 - sizes.get('--avatar-border').value);
-		  const avatarTargetTranslate = maxScroll - avatarScrollEndPos;
-		  // Stop scaling at this offset and start transform.
-		  const avatarScrollEndOffset = avatarScrollEndPos / maxScroll;
-
-		  const aekf = avatarEffect.getKeyframes();
-		  aekf[1].transform = `translateY(0px) scale(${avatarTargetScale})`;
-		  aekf[1].offset = avatarScrollEndOffset;
-		  aekf[2].transform = `translateY(${avatarTargetTranslate}px) scale(${avatarTargetScale})`;
-		  avatarEffect.setKeyframes(aekf);
-
-		  const bekf = barEffect.getKeyframes();
-		  bekf[1].offset = avatarScrollEndOffset;
-		  barEffect.setKeyframes(bekf);
+			this.initAvatarEffect();
+			this.initBarEffect();
+			this.update();
+			window.addEventListener('resize', _ => this.update());
+		}catch(e){
+			console.log('Will Use Polyfill :\'(');
 		}
-		update();
-		window.addEventListener('resize', _ => update());
 
 		/* crbug(824782): delay is not working as expected in worklet, instead here we combine
 		   what would have been a delayed animation with the other avatar animation but start
@@ -117,6 +61,76 @@ export class AnimationHeader{
 			{}
 		  )//.play();
 		*/
+	}
+
+	update() {
+		const clientHeight = this.scrollSource.clientHeight;
+		const scrollHeight = this.scrollSource.scrollHeight;
+		const maxScroll = scrollHeight - clientHeight;
+		console.log(clientHeight, scrollHeight, maxScroll);
+		const avatarTargetScale = this.sizes.get('--bar-height').value / (this.sizes.get('--avatar-size').value + 2 * this.sizes.get('--avatar-border').value);
+		const avatarScrollEndPos = (this.sizes.get('--header-height').value/2 - this.sizes.get('--avatar-size').value/2 - this.sizes.get('--avatar-border').value);
+		const avatarTargetTranslate = maxScroll - avatarScrollEndPos;
+		// Stop scaling at this offset and start transform.
+		const avatarScrollEndOffset = avatarScrollEndPos / maxScroll;
+
+		const aekf = this.avatarEffect.getKeyframes();
+		aekf[1].transform = `translateY(0px) scale(${avatarTargetScale})`;
+		aekf[1].offset = avatarScrollEndOffset;
+		aekf[2].transform = `translateY(${avatarTargetTranslate}px) scale(${avatarTargetScale})`;
+		this.avatarEffect.setKeyframes(aekf);
+
+		const bekf = this.barEffect.getKeyframes();
+		bekf[1].offset = avatarScrollEndOffset;
+		this.barEffect.setKeyframes(bekf);
+	}
+
+	initAvatarEffect() {
+		this.avatarEffect = new KeyframeEffect(this.avatar, [
+			{transform: `translateY(0px) scale(1)`, easing: 'ease-in-out', offset: 0},
+			{transform: `translateY(0px) scale(${0/*avatarTargetScale*/})`, easing: 'linear', offset: 0 /*avatarScrollEndOffset*/},
+			{transform: `translateY(${0/*avatarTargetTranslate*/}px) scale(${0/*avatarTargetScale*/})`, offset: 1},
+		], {
+			duration: this.maxTime + this.epsilon,
+			fill: 'both',
+			iterations: Infinity,
+		});
+
+		new WorkletAnimation('animator-header',
+			this.avatarEffect,
+			this.scrollTimeline,
+			[]
+		).play();
+	}
+
+	initBarEffect() {
+		this.barEffect = new KeyframeEffect(
+			this.bar,
+			[
+			{opacity: 0, offset: 0},
+			{opacity: 1, offset: 0 /*avatarScrollEndOffset*/},
+			{opacity: 1, offset: 1}
+			],
+			{
+			duration: this.maxTime + this.epsilon,
+			fill: 'both',
+			iterations: Infinity,
+			/* crbug(779189): Use infinity iteration and maxDuration to avoid effect
+				prematurely finishing.
+
+				BTW, Web Animations uses an endpoint-exclusive timing model, which mean
+				when timeline is at "duration" time, it is considered to be at 0 time of the
+				second iteration. To avoid this, we ensure our max time (max scroll offset) never
+				reaches duration by having duration an epsilon larger.  This hack is not
+				needed once we fix the original bug above.
+				*/
+			}
+		);
+		new WorkletAnimation('animator-header',
+			this.barEffect,
+			this.scrollTimeline,
+			[]
+		).play();
 	}
 
 }
