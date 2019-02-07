b0f53d04b2695b4fce2f6a3a0cf6e6a73993eb08
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2019-02-07 10:33:05 +0100

    Add AutoML Talk

---
+++ Prezs/AutoML/assets/markdown/prez.md
@@ -0,0 +1,205 @@
+
+<!-- .slide: class="first-slide" -->
+
+# **AutoML**
+
+
+### Ou comment faire du machine learning sans data Scientist
+
+
+##==##
+
+# Disclamer ⚠️
+
+![center w-500](./assets/images/cat-meme.jpg)
+
+
+##==##
+
+<!-- .slide: data-background="./assets/images/jeshoots-com-436787-unsplash.jpg" class="transition text-white"  -->
+
+# Avant
+
+##==##
+
+## Machine Learning process
+
+![center w-700](./assets/images/Machine_learning.png)
+
+Notes:
+Fallait tout hoster, tout entrainer, ...
+
+##==##
+
+
+<!-- .slide: data-background="./assets/images/tensorflow.jpeg" class="no-filter"  -->
+
+
+##==##
+
+## Tensorflow process
+
+![center w-700](./assets/images/Machine_learning_Tensorflow.png)
+
+Notes:
+Ici les algos sont déjà écrits mais reste à gérer soit même l'entrainement etc...
+
+
+##==##
+
+<!-- .slide: class="no-filter transition"  -->
+
+# Apis existantes
+
+![center w-700](./assets/images/google-cloud-api.png)
+
+Notes:
+La on parle de google mais ça marche aussi pour Microsoft, IBM, Amazon ...
+Chacun propose la sienne, déjà pré-entrainée, ...
+
+
+##==##
+
+## CloudML Api
+
+![center w-700](./assets/images/Machine_learning_visionapi.png)
+
+Notes:
+Ici tout est pret pour nous et exposé sous forme d'API Rest
+
+
+##==##
+
+
+<!-- .slide: data-background="./assets/images/groot-hodor-chewbacca.jpg" class="transition text-red"  -->
+
+
+# Et si je veux mon propre modèle ?
+
+Notes:
+Je s'appelle Groot
+
+##==##
+
+<!-- .slide: class="no-filter transition"  -->
+
+# AutoML à la rescousse
+
+![center w-700](./assets/images/auto_ml.png)
+
+
+##==##
+
+## Automl process
+
+![center w-700](./assets/images/Machine_learning_automl.png)
+
+
+Notes:
+ici, on a plus qu'à gérer le dataset
+
+##==##
+
+<!-- .slide: class="no-filter transition"  -->
+
+# Autre APIS
+
+![center w-700](./assets/images/api-lead.png)
+
+Translation | Natural Languages | Vision
+
+Notes:
+Natural Langages / Translation
+
+
+##==##
+
+<!-- .slide: data-background="./assets/images/alen-jacob-589057-unsplash.jpg" class="transition text-red"  -->
+
+# Under the hood
+
+
+##==##
+
+
+![center h-500](./assets/images/creating-ml-solutions_2x.png)
+
+Notes:
+Tout se joue sur l'utilisation de models pré-entrainés et dédiés à des types de données
+
+
+##==##
+
+
+
+# Génération de datasets
+
+![center w-700](./assets/images/automl_datasets.png)
+
+Notes:
+Pricing dépendant du nombre d'images dans le datasets (3$ toutes les 1000 images)
+
+##==##
+
+
+
+# Détail d'un dataset
+
+![center w-700](./assets/images/automl_dataset_detail.png)
+
+##==##
+
+
+
+# Entrainement d'un modèle
+
+![center w-700](./assets/images/automl_model_training.png)
+
+##==##
+
+
+
+# Matrice de confusion
+
+![center w-700](./assets/images/automl_confusion_matrix.png)
+
+##==##
+
+
+
+# Test du modèle
+
+![center w-700](./assets/images/automl_training.png)
+
+
+##==##
+
+
+
+<!-- .slide: class="no-filter transition"  -->
+
+# Autres solutions
+
+![center w-700](./assets/images/watson_studio.png)
+
+IBM Watson Studio
+
+##==##
+
+<!-- .slide: class="who-am-i" -->
+
+## Des Questions ?
+
+### Jean-François Garreau
+
+<!-- .element: class="descjf" -->
+GDE Web Technologies,  Front-end developer & Community Manager
+
+![avatar w-300 wp-200](assets/images/jf.jpg)
+
+
+![company_logo](assets/images/logo-SFEIR-blanc-orange.svg)
+![gdg_logo](assets/images/GDG-Logo-carre.png)
+
+<!-- .element: class="twitter" -->
+[@jefBinomed](https://twitter.com/jefBinomed)
---
+++ Prezs/AutoML/scripts/demos.js
@@ -0,0 +1,159 @@
+'use strict'
+
+
+class MediaCaptureDemo {
+    constructor({
+        video
+    }) {
+        this._stream = null;
+        this.video = video;
+        this.track = null;
+        this.imageCapture = null;
+
+    }
+
+    set stream(stream) {
+        this._stream = stream;
+        this.track = stream.getVideoTracks()[0];
+        this.imageCapture = new ImageCapture(this.track);
+    }
+
+    get stream() {
+        return this._stream;
+    }
+}
+
+export class Demos {
+    constructor() {
+        this._detectingLabels('', this._callVisionApi);
+        this._detectingLabels('automl-', this._callAutomlApi);
+    }
+
+    /***
+     * Generic method to get video and drag and drop management
+     * @param prefix: the prefix to target elements in the html
+     * @param analyzeMethod: the method called to analyze the blob
+     */
+    _detectingLabels(prefix, analyzeMethod) {
+
+        // Video 
+        const video = document.getElementById(`${prefix}mirror-label`);
+        const mediaCaptureDemo = new MediaCaptureDemo({
+            video
+        });
+        // Drag & drop
+        const targetDrop = document.getElementById(`${prefix}targetVision`);
+        // Result element
+        const labelDetected = document.getElementById(`${prefix}labels-detected`);
+
+        const elementToReduceArray = [video, targetDrop];
+
+        // Start the camera
+        document.getElementById(`${prefix}startVideo`).addEventListener('click', () => {
+            targetDrop.classList.add('hide');
+            video.classList.remove('hide');
+
+            navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: false
+                })
+                .then((stream) => {
+                    mediaCaptureDemo.stream = stream;
+                    if (navigator.mozGetUserMedia) {
+                        mediaCaptureDemo.video.mozSrcObject = stream;
+                    } else {
+                        let vendorURL = window.URL || window.webkitURL;
+                        mediaCaptureDemo.video.src = vendorURL.createObjectURL(stream);
+                    }
+                    mediaCaptureDemo.video.play();
+                })
+                .catch((err) => {
+                    console.log("An error occured! " + err);
+                });
+        });
+        
+        // Take a picture using media capture api
+        document.getElementById(`${prefix}takeAPicture`).addEventListener('click', () => {
+            mediaCaptureDemo.imageCapture.takePhoto()
+                .then(blob => {
+                    analyzeMethod(elementToReduceArray, labelDetected, blob, 'shirt');
+                })
+        });
+
+
+        // Drag & Drop management
+        targetDrop.addEventListener('drop', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            const file = e.dataTransfer.files[0];
+            targetDrop.innerHTML = `<img src='${URL.createObjectURL(file)}' >`
+            analyzeMethod(elementToReduceArray, labelDetected, file, 'train');
+        });
+        targetDrop.addEventListener('dragover', (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            e.dataTransfer.dropEffect = 'copy';
+        })
+    }
+
+
+    /**
+     * Call Google Vision Api
+     * @param {HTMLElement} elementToReduce 
+     * @param {HTMLElement} targetElement 
+     * @param {Blob} blob 
+     */
+    _callVisionApi(elementToReduce, targetElement, blob) {
+        const formData = new FormData();
+        formData.append('blob', blob);
+        fetch('http://localhost:8000/vision', {
+                method: 'POST',
+                mode: 'cors',
+                body: formData
+            }).then((tojson) => tojson.json())
+            .then((results) => {
+                elementToReduce.forEach(element => element.classList.add('with-label'));
+                targetElement.classList.remove('hide');
+                let resultStr = '';
+                results.forEach((annotationResult, index) => {
+                    resultStr += `Result : ${index} \n<ul>`;
+                    annotationResult.labelAnnotations.forEach(annotation => {
+                        resultStr += `<li>${annotation.description} : ${annotation.score}</li>`;
+                    });
+                    resultStr += '</ul>'
+                });
+                targetElement.innerHTML = resultStr;
+            });
+    }
+    
+    /**
+     * Call Google Vision Api
+     * @param {HTMLElement} elementToReduce 
+     * @param {HTMLElement} targetElement 
+     * @param {Blob} blob 
+     * @param {String} type
+     */
+    _callAutomlApi(elementToReduce, targetElement, blob, type) {
+        const formData = new FormData();
+        formData.append('blob', blob);
+        fetch(`http://localhost:8000/automl?model=${type}`, {
+                method: 'POST',
+                mode: 'cors',
+                body: formData
+            }).then((tojson) => tojson.json())
+            .then((response) => {
+                const results = response.responses;
+                elementToReduce.forEach(element => element.classList.add('with-label'));
+                targetElement.classList.remove('hide');
+                let resultStr = '';
+                results.forEach((annotationResult, index) => {
+                    resultStr += `Result : ${index} \n<ul>`;
+                    annotationResult.customLabelAnnotations.forEach(annotation => {
+                        resultStr += `<li>${annotation.label} : ${annotation.score}</li>`;
+                    });
+                    resultStr += '</ul>'
+                });
+                targetElement.innerHTML = resultStr;
+            });
+    }
+}
