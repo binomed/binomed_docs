756063f8eb221ae86cc39a4f620490e838bcf4c9
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2019-04-16 23:53:06 +0200

    houdini : Push new style

--- Prezs/HoudiniCss/scripts/animations/anim.js
+++ Prezs/HoudiniCss/scripts/animations/anim.js
@@ -4,6 +4,8 @@ export class Animations {
 		this._browserEngine();
 
 		this._animateHoudini();
+
+		this._workletSteps();
 	}
 
 	_browserEngine() {
@@ -109,5 +111,68 @@ export class Animations {
                 Reveal.removeEventListener('fragmentshown', callBackFragment);
             }
         });
+	}
+
+	_workletSteps() {
+		let stepAnimation = 0;
+		const STEP_ADD = 1;
+		const STEP_ADD_WORKKET = 2;
+		const STEP_LOAD = 3;
+		const STEP_RENDER = 4;
+		const STEP_CALL = 5;
+		const STEP_EXECUTE = 6;
+
+		function fragmentAnimation(){
+			stepAnimation++;
+			switch(stepAnimation){
+				case(STEP_ADD):{
+					document.querySelector('#worklet-diagram .worklet-step-1').classList.add('show');
+					break;
+				}
+				case(STEP_ADD_WORKKET):{
+					document.querySelector('#worklet-diagram .worklet-step-2').classList.add('show');
+					break;
+				}
+				case(STEP_LOAD):{
+					document.querySelector('#worklet-diagram .worklet-step-3').classList.add('show');
+					break;
+				}
+				case(STEP_RENDER):{
+					document.querySelector('#worklet-diagram .worklet-step-4').classList.add('show');
+					break;
+				}
+				case(STEP_CALL):{
+					document.querySelector('#worklet-diagram .worklet-step-5').classList.add('show');
+					break;
+				}
+				case(STEP_EXECUTE):{
+					document.querySelector('#worklet-diagram .worklet-step-6').classList.add('show');
+					break;
+				}
+
+			}
+		}
+
+		Reveal.addEventListener('worklet-steps', ()=>{
+			Reveal.addEventListener('fragmentshown', fragmentAnimation);
+			stepAnimation = 0;
+
+			function clearAnim(){
+				Reveal.removeEventListener('fragmentshown', fragmentAnimation);
+				Reveal.removeEventListener('slidechanged', clearAnim);
+				document.querySelector('#worklet-diagram .worklet-step-1').classList.remove('show');
+				document.querySelector('#worklet-diagram .worklet-step-2').classList.remove('show');
+				document.querySelector('#worklet-diagram .worklet-step-3').classList.remove('show');
+				document.querySelector('#worklet-diagram .worklet-step-4').classList.remove('show');
+				document.querySelector('#worklet-diagram .worklet-step-5').classList.remove('show');
+				document.querySelector('#worklet-diagram .worklet-step-6').classList.remove('show');
+
+			}
+
+			setTimeout(()=>{
+				Reveal.addEventListener('slidechanged', clearAnim);
+			},100);
+		});
+
 	}
 }
--- Prezs/HoudiniCss/scripts/houdini/animation-header.js
+++ Prezs/HoudiniCss/scripts/houdini/animation-header.js
@@ -13,48 +13,106 @@ export class AnimationHeader{
 			  initialValue: '0px',
 			  inherits: true
 			});
-		  });
+			});
+		let animationWorklet = window.animationWorklet;
+		if (CSS)
+			animationWorklet = CSS.animationWorklet;
 
-		await animationWorklet.addModule('./scripts/houdini/animator-header.js');
-
-		const sizes = document.querySelector('#demoAnimationWorklet').computedStyleMap();
-		const scrollSource = document.querySelector('#demoAnimationWorklet');
-		const avatar = document.querySelector('#demoAnimationWorklet .avatar');
-		const bar = document.querySelector('#demoAnimationWorklet .bar');
-		const maxTime = 1000;
-		const epsilon = 1e-2;
-		const scrollTimeline = new ScrollTimeline({
-		  scrollSource,
-		  orientation: 'block',
-		  timeRange: maxTime,
+		this.sizes = document.querySelector('#demoAnimationWorklet').computedStyleMap();
+		this.scrollSource = document.querySelector('#demoAnimationWorklet');
+		this.avatar = document.querySelector('#demoAnimationWorklet .avatar');
+		this.bar = document.querySelector('#demoAnimationWorklet .bar');
+		this.maxTime = 1000;
+		this.epsilon = 1e-2;
+		this.scrollTimeline = new ScrollTimeline({
+			scrollSource: this.scrollSource,
+			orientation: 'block',
+			timeRange: this.maxTime,
 		});
-		// console.log(avatarScrollEndPos, avatarScrollEndOffset);
-		// console.log(scrollSize);
-		const avatarEffect = new KeyframeEffect(avatar, [
-		  {transform: `translateY(0px) scale(1)`, easing: 'ease-in-out', offset: 0},
-		  {transform: `translateY(0px) scale(${0/*avatarTargetScale*/})`, easing: 'linear', offset: 0 /*avatarScrollEndOffset*/},
-		  {transform: `translateY(${0/*avatarTargetTranslate*/}px) scale(${0/*avatarTargetScale*/})`, offset: 1},
+		try{
+
+
+			await animationWorklet.addModule('./scripts/houdini/animator-header.js');
+
+			this.initAvatarEffect();
+			this.initBarEffect();
+			this.update();
+			window.addEventListener('resize', _ => this.update());
+		}catch(e){
+			console.log('Will Use Polyfill :\'(');
+		}
+
+		/* crbug(824782): delay is not working as expected in worklet, instead here we combine
+		   what would have been a delayed animation with the other avatar animation but start
+		   it at a different offset.
+		*/
+		  new WorkletAnimation('twitter-header',
+			[
+			  new KeyframeEffect(this.avatar, [
+				{transform: `translateY(0px)`},
+				{transform: `translateY(${this.scrollHeight - this.clientHeight}px)`},
+			  ], {
+				delay: this.avatarScrollEndPos/this.scrollHeight * this.maxTime,
+				duration: (this.scrollHeight - this.clientHeight)/this.scrollHeight * this.maxTime,
+				fill: 'both',
+			  }),
+			],
+			this.scrollTimeline,
+			{}
+		  ).play();
+		/**/
+	}
+
+	update() {
+		this.clientHeight = this.scrollSource.clientHeight;
+		this.scrollHeight = this.scrollSource.scrollHeight;
+		this.maxScroll = this.scrollHeight - this.clientHeight;
+		console.log(this.clientHeight, this.scrollHeight, this.maxScroll);
+		this.avatarTargetScale = this.sizes.get('--bar-height').value / (this.sizes.get('--avatar-size').value + 2 * this.sizes.get('--avatar-border').value);
+		this.avatarScrollEndPos = (this.sizes.get('--header-height').value/2 - this.sizes.get('--avatar-size').value/2 - this.sizes.get('--avatar-border').value);
+		this.avatarTargetTranslate = this.maxScroll - this.avatarScrollEndPos;
+		// Stop scaling at this offset and start transform.
+		this.avatarScrollEndOffset = this.avatarScrollEndPos / this.maxScroll;
+
+		const aekf = this.avatarEffect.getKeyframes();
+		aekf[1].transform = `translateY(0px) scale(${this.avatarTargetScale})`;
+		aekf[1].offset = this.avatarScrollEndOffset;
+		aekf[2].transform = `translateY(${this.avatarTargetTranslate}px) scale(${this.avatarTargetScale})`;
+		this.avatarEffect.setKeyframes(aekf);
+
+		const bekf = this.barEffect.getKeyframes();
+		bekf[1].offset = this.avatarScrollEndOffset;
+		this.barEffect.setKeyframes(bekf);
+	}
+
+	initAvatarEffect() {
+		this.avatarEffect = new KeyframeEffect(this.avatar, [
+			{transform: `translateY(0px) scale(1)`, easing: 'ease-in-out', offset: 0},
+			{transform: `translateY(0px) scale(${0/*this.avatarTargetScale*/})`, easing: 'linear', offset: 0 /*this.avatarScrollEndOffset*/},
+			{transform: `translateY(${0/*this.avatarTargetTranslate*/}px) scale(${0/*this.avatarTargetScale*/})`, offset: 1},
 		], {
-		  duration: maxTime + epsilon,
-		  fill: 'both',
-		  iterations: Infinity,
+			duration: this.maxTime + this.epsilon,
+			fill: 'both',
+			iterations: Infinity,
 		});
 
 		new WorkletAnimation('animator-header',
-		  [avatarEffect],
-		  scrollTimeline,
-		  {}
+			this.avatarEffect,
+			this.scrollTimeline,
+			[]
 		).play();
+	}
 
-		const barEffect = new KeyframeEffect(
-		  bar,
-		  [
+	initBarEffect() {
+		this.barEffect = new KeyframeEffect(
+			this.bar,
+			[
 			{opacity: 0, offset: 0},
 			{opacity: 1, offset: 0 /*avatarScrollEndOffset*/},
 			{opacity: 1, offset: 1}
-		  ],
-		  {
-			duration: maxTime + epsilon,
+			],
+			{
+			duration: this.maxTime + this.epsilon,
 			fill: 'both',
 			iterations: Infinity,
 			/* crbug(779189): Use infinity iteration and maxDuration to avoid effect
@@ -65,58 +123,14 @@ export class AnimationHeader{
 				second iteration. To avoid this, we ensure our max time (max scroll offset) never
 				reaches duration by having duration an epsilon larger.  This hack is not
 				needed once we fix the original bug above.
-			  */
-		  }
+				*/
+			}
 		);
 		new WorkletAnimation('animator-header',
-		  [barEffect],
-		  scrollTimeline,
-		  {}
+			this.barEffect,
+			this.scrollTimeline,
+			[]
 		).play();
-
-		function update() {
-		  const clientHeight = scrollSource.clientHeight;
-		  const scrollHeight = scrollSource.scrollHeight;
-		  const maxScroll = scrollHeight - clientHeight;
-		  console.log(clientHeight, scrollHeight, maxScroll);
-		  const avatarTargetScale = sizes.get('--bar-height').value / (sizes.get('--avatar-size').value + 2 * sizes.get('--avatar-border').value);
-		  const avatarScrollEndPos = (sizes.get('--header-height').value/2 - sizes.get('--avatar-size').value/2 - sizes.get('--avatar-border').value);
-		  const avatarTargetTranslate = maxScroll - avatarScrollEndPos;
-		  // Stop scaling at this offset and start transform.
-		  const avatarScrollEndOffset = avatarScrollEndPos / maxScroll;
-
-		  const aekf = avatarEffect.getKeyframes();
-		  aekf[1].transform = `translateY(0px) scale(${avatarTargetScale})`;
-		  aekf[1].offset = avatarScrollEndOffset;
-		  aekf[2].transform = `translateY(${avatarTargetTranslate}px) scale(${avatarTargetScale})`;
-		  avatarEffect.setKeyframes(aekf);
-
-		  const bekf = barEffect.getKeyframes();
-		  bekf[1].offset = avatarScrollEndOffset;
-		  barEffect.setKeyframes(bekf);
-		}
-		update();
-		window.addEventListener('resize', _ => update());
-
-		/* crbug(824782): delay is not working as expected in worklet, instead here we combine
-		   what would have been a delayed animation with the other avatar animation but start
-		   it at a different offset.
-
-		  new WorkletAnimation('twitter-header',
-			[
-			  new KeyframeEffect(avatar, [
-				{transform: `translateY(0px)`},
-				{transform: `translateY(${scrollHeight - clientHeight}px)`},
-			  ], {
-				delay: avatarScrollEndPos/scrollHeight * maxTime,
-				duration: (scrollHeight - clientHeight)/scrollHeight * maxTime,
-				fill: 'both',
-			  }),
-			],
-			scrollTimeline,
-			{}
-		  )//.play();
-		*/
 	}
 
 }
